cd5715fc936a765b75e7906631369f680d708007
kei95 <kei0613shine.star@gmail.com>
2024-06-19 10:34:06 -0700

    Add update fe-app

--- fe-app/src/App.tsx
+++ fe-app/src/App.tsx
@@ -1,9 +1,10 @@
+import { yassifyJSR } from "@kei95/yassify-jsr";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>{"Hello VanJS"}</h1>
+      <h1>{yassifyJSR("Hello VanJS")}</h1>
     </>
   );
 }
